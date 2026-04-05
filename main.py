c4e69060f1a8e567c5b26aa4a54c2a289d5be7e2
rune_lol <technonerd2006@gmail.com>
2026-04-05 22:25:42 +0200

    Functional Solver :0

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import math
 
-from tracktools import get_track
+from tracktools import get_track, draw_track, calcTotalTime, getTotalDistance
 
 G = 9.81 #m/s^2
+rho_air = 1.225 #kg/m^3
 
 car = {
     'mu': 0.8, #mu
     'P_max': 100_000, #W
     'm': 1000, #kg
+    'ClA': -2.5 #m^2 * Cl (high downforce)
 }
+
+def get_local_vmax(kappa):
+    return math.sqrt((car['mu']*G)/abs(kappa))
 
 def get_max_corner_speeds(track):
     """a_lat = v^2 * kappa"""
-    v_max = []*len(track)
-    for i in track:
-        v_max[i] = math.sqrt((G*car['mu'])/track[1])
+    v_max = [0]*len(track)
+    print(len(track), len(v_max))
+    for i, segment in enumerate(track):
+        try:
+            v_max[i] = get_local_vmax(segment[1])
+        except ZeroDivisionError:
+            v_max[i] = 999_999_999 # absurdly high speed to cap, no car will ever reach
 
     return v_max
 
@@ -23,6 +32,54 @@ def main():
     track = get_track()
 
     v_max = get_max_corner_speeds(track)
-    print(v_max)
 
-    pass
+    # actual solver
+    v = v_max.copy()
+
+    v[0] = 0 #start speed (from a dig)
+    epoch = 1
+    while epoch <= 100:
+        forward_propagation(track, v)
+        backward_propagation(track, v)
+        
+        print('Ran',epoch,'epochs')
+        epoch += 1
+
+    print('t:',calcTotalTime(track, v),'s')
+    print('s:',getTotalDistance(track),'m')
+    print('v_avg:',(getTotalDistance(track)/calcTotalTime(track, v))*3.6,'km/h')
+    draw_track(track, v_max)
+    draw_track(track, v)
+
+def backward_propagation(track, v):
+    for j, v_seg in enumerate(reversed(v)):
+        i = len(v)-j-1
+        F_lat = car['m']*(v_seg**2)*track[i][1]
+        F_total_available = car['mu']*car['m']*G
+        F_longLeftover = math.sqrt(max(F_total_available**2 - F_lat**2, 0))
+
+        accel_brake = -F_longLeftover/car['m']
+        v_prev = math.sqrt(v_seg**2 - 2*accel_brake*track[-i][0])
+        if i-1 >= 0:
+            v[i-1] = min(v[i-1], v_prev)
+
+def forward_propagation(track, v):
+    for i, v_seg in enumerate(v):
+        F_lat = car['m']*(v_seg**2)*track[i][1]
+        F_total_available = car['mu']*car['m']*G
+        F_longLeftover = math.sqrt(max(F_total_available**2 - F_lat**2, 0))
+        
+        accel_grip_limited = F_longLeftover/car['m']
+        try:
+            accel_power_limited = car['P_max']/(abs(v_seg)*car['m'])
+        except ZeroDivisionError:
+            accel_power_limited = accel_grip_limited
+        accel = min(accel_grip_limited, accel_power_limited)
+        v_next = math.sqrt(v_seg**2 + 2*accel*track[i][0])
+        try:
+            v[i+1] = min(v[i+1], v_next)
+        except IndexError:
+            pass
+
+if __name__ == "__main__":
+    main()
--- tracktools.py
+++ tracktools.py
@@ -1,11 +1,65 @@
+import math
+
+import matplotlib.pyplot as plt
+
 def get_track():
     track = []
     for _ in range(0, 100):
         track.append((1, 0)) # ds (1m), kappa (1/r)
     for _ in range(0, 100):
         track.append((1, 0.01)) # ds (1m), kappa (1/r) (1/100)
-    for i in range(0, 100):
-        track.append((0.1, 0.01 - i*0.01)) # ds (1m), kappa (1/r) (1/100 - 1/9999999)
+    for i in range(0, 50):
+        track.append((1, -0.001 + i*0.001)) # ds (1m), kappa (1/r) (1/100 - 1/9999999)
+    for i in range(0, 50):
+        track.append((1, 0.001 - i*0.001)) # ds (1m), kappa (1/r) (1/100 - 1/9999999)
+    for _ in range(0, 100):
+        track.append((1, 0.01)) # ds (1m), kappa (1/r) (1/100)
+    for _ in range(0, 100):
+        track.append((1, 0)) # ds (1m), kappa (1/r)
     
-
     return track
+
+def draw_track(track, speed=None):
+    if speed is None:
+        speed = [0]*len(track)
+
+    heading = [0]*len(track)
+    prev_heading = 0
+    for i, seg in enumerate(track):
+        heading[i] = seg[1]*seg[0] + prev_heading
+        prev_heading = heading[i]
+
+    x = []
+    y = []
+    lastCoord = (0, 0)
+    for i, _ in enumerate(track):
+        x_n = lastCoord[0] + track[i][0]*math.cos(heading[i])
+        y_n = lastCoord[1] + track[i][0]*math.sin(heading[i])
+        x.append(x_n)
+        y.append(y_n)
+        new_coord = (x_n, y_n)
+        lastCoord = new_coord
+
+    plt.figure()
+    plt.scatter(x, y, c=speed, cmap='plasma')
+    plt.colorbar(label="Speed")
+    plt.axis('equal')
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+    plt.title("Track layout")
+    plt.show()
+
+def calcTotalTime(track, speedmap):
+    t = 0
+    for i, seg in enumerate(track):
+        try:
+            t += seg[0]/speedmap[i]
+        except ZeroDivisionError:
+            print('Passing ZeroDivisionError in the total time calculator')
+    return t
+
+def getTotalDistance(track):
+    s = 0
+    for seg in track:
+        s += seg[0]
+    return s
