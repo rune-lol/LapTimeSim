6d51e2a414daa42c49e463b6c6ea397c7f193cf8
rune_lol <technonerd2006@gmail.com>
2026-04-09 23:58:04 +0200

    Working Power train model, aswell as slightly improved visualisation

--- carModel.py
+++ carModel.py
@@ -1,25 +1,74 @@
 import math
 from dataclasses import dataclass
+import numpy as np
 
 from physical_constants import RHO_AIR, G
+from powertrain_tools import get_coefficients
 
 STRAIGHT_LINE_CAP = 1_000.0
 
 @dataclass
 class CarModel:
-    mu: float = 0 # tyre coefficient of friction
-    P_max: float = 0 # maximum power in watts
-    m: float = 0 # mass in kilograms
-    ClA: float = 0 # Cl * A, m^2, negative = downforce
-    CdA: float = 0 # Cd * A, m^2
+    mu: float # tyre coefficient of friction
+    P_max: float # maximum power in watts
+    m: float # mass in kilograms
+    ClA: float # Cl * A, m^2, negative = downforce
+    CdA: float # Cd * A, m^2
 
-    #def fromDict(self, inputDict: dict):
-    #    """Legacy code"""
-    #    self.mu = inputDict['mu']
-    #    self.P_max = inputDict['P_max']
-    #    self.m = inputDict['m']
-    #    self.ClA = inputDict['ClA']
-    #    self.CdA = inputDict['CdA']
+    # engine specs
+    idlerpm: int
+    redlinerpm: int
+    torque_coefficients: np.ndarray
+    T_max: float
+
+    # gearbox specs
+    gear_ratios: list[float]
+    final_drive: float
+    trans_efficiency: float
+    driven_wheel_radius: float
+    shift_point: int
+
+    def get_optimal_gear(self, speed):
+        # Assume shifting at the same speed for every gear
+        gear = 1
+        for ratio in self.gear_ratios:
+            rpm = (speed / (self.driven_wheel_radius * 2 * math.pi))*60*ratio*self.final_drive
+            if rpm < self.shift_point:
+                break
+            else:
+                gear += 1
+
+        gear = np.clip(gear, 1, len(self.gear_ratios))
+        return gear
+
+    def get_optimal_rpm(self, speed):
+        return (speed / (self.driven_wheel_radius * 2 * math.pi))*60*self.gear_ratios[self.get_optimal_gear(speed)-1]*self.final_drive
+
+    def get_max_usable_engine_wheel_force(self, speed):
+        rpm = self.get_optimal_rpm(speed)
+        engTorque = self.get_torque(rpm)
+        wheelTorque = engTorque * self.final_drive * self.gear_ratios[self.get_optimal_gear(speed)-1]
+        return wheelTorque / self.driven_wheel_radius
+
+    def get_torque(self, rpm):
+        if rpm < self.idlerpm:
+            return 0.6*self.T_max
+        if rpm >= self.redlinerpm:
+            return 0
+
+        poly = np.poly1d(self.torque_coefficients)
+
+        rpm = np.asarray(rpm)  # ensures array operations work
+        rpm_norm = rpm # normalize RPM to match polynomial fitting
+        torque = poly(rpm_norm) * self.T_max  # scale to peak torque
+        torque = np.clip(torque, 0, self.T_max)     # can't go below 0 or above peak
+        return torque
+
+    def get_power(self, rpm):
+        torque = self.get_torque(rpm)
+        omega = (rpm/60)*2*np.pi
+        power = torque * omega
+        return power
 
     def get_local_vmax(self, kappa):
         if kappa < 1e-6:
@@ -32,7 +81,6 @@ class CarModel:
 
         v_max = math.sqrt((self.mu*self.m*G)/denom)
         return v_max
-    
     
     def get_max_corner_speeds(self, track):
         """a_lat = v^2 * kappa"""
@@ -54,5 +102,7 @@ class CarModel:
 
 @dataclass
 class CarState:
+    gear: int = 0
+    rpm: int = 0
     forces: tuple[float, float] = (0, 0) # Lateral, Longtitudinal, in Newton
     v: float = 0 # speed in m/s
--- tracktools.py
+++ tracktools.py
@@ -12,8 +12,6 @@ def get_track(trackFileLocation):
     
     data = pd.read_csv(trackFileLocation)
 
-    print(data.head)
-
     x = data["# x_m"].to_numpy()
     y = data["y_m"].to_numpy()
     
@@ -36,43 +34,6 @@ def get_track(trackFileLocation):
         track.append((dS, kappa))
 
     return track
-
-def draw_track(track, car_state=None):
-    speed = []
-    if type(car_state[0]) == CarState:
-        for state in car_state:
-            speed.append(state.v)
-    elif type(car_state[0]) == float:
-        speed = car_state
-
-    if speed is None:
-        speed = [0]*len(track)
-
-    heading = [0]*len(track)
-    prev_heading = 0
-    for i, seg in enumerate(track):
-        heading[i] = seg[1]*seg[0] + prev_heading
-        prev_heading = heading[i]
-
-    x = []
-    y = []
-    lastCoord = (0, 0)
-    for i, _ in enumerate(track):
-        x_n = lastCoord[0] + track[i][0]*math.cos(heading[i])
-        y_n = lastCoord[1] + track[i][0]*math.sin(heading[i])
-        x.append(x_n)
-        y.append(y_n)
-        new_coord = (x_n, y_n)
-        lastCoord = new_coord
-
-    plt.figure(figsize=(6, 3))
-    plt.scatter(x, y, c=speed, cmap='plasma')
-    plt.colorbar(label="Speed")
-    plt.axis('equal')
-    plt.xlabel("x [m]")
-    plt.ylabel("y [m]")
-    plt.title("Track Speed")
-    plt.show()
 
 def calcTotalTime(track, car_states):
     t = 0
